acb44e238f95730efe2cb1998d88bd2a42869447
Prashant Mishra <prashant24.juit@gmail.com>
2016-11-05 14:49:13 -0400

    dfh

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
     root "welcome#index"
-    get '*path' => redirect('/')
-    post '*path' => redirect('/')
+    get '*path' => "welcome#index"
+    post '*path' => "welcome#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
